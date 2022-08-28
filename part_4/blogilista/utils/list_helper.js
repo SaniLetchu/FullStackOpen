8836bcc3c84ffd5eec7370e4cbf39ff510952621
Sani Letchu <sankku@MacBook-Air.home>
2022-08-28 22:01:00 +0300

    saved

--- part_4/blogilista/utils/list_helper.js
+++ part_4/blogilista/utils/list_helper.js
@@ -2,6 +2,26 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let likes = 0
+  blogs.forEach(blog => {
+    likes += blog.likes
+  })
+  return likes
+}
+
+const favoriteBlog = (blogs) => {
+  let mostLikes = blogs[0]
+  blogs.forEach(blog => {
+    if(mostLikes.likes < blog.likes) {
+      mostLikes = blog
+    }
+  })
+  return mostLikes
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
