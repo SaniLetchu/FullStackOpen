1dc32156f8ecca936bfe02ea0a6e73645876111d
Sani Letchu <sankku@Sanis-Air.home>
2022-08-29 21:45:42 +0300

    save progress

--- part_4/blogilista/utils/list_helper.js
+++ part_4/blogilista/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -20,8 +22,41 @@ const favoriteBlog = (blogs) => {
   return mostLikes
 }
 
+const mostBlogs = (blogs) => {
+  const nameOfAuthor = (blog => {
+    return blog.author
+  })
+  const lengthOfArray = (array => {
+    return array[1].length
+  })
+  const groupByAuthor = Object.entries(_.groupBy(blogs, nameOfAuthor))
+  const authorWithMostBlogs = _.maxBy(groupByAuthor, lengthOfArray)
+  return {
+    author: authorWithMostBlogs[0],
+    blogs: authorWithMostBlogs[1].length
+  }
+}
+
+const mostLikes = (blogs) => {
+  const nameOfAuthor = (blog => {
+    return blog.author
+  })
+  const sumOfLikes = (array => {
+    const likes = (object => object.likes)
+    return _.sumBy(array[1], likes)
+  })
+  const groupByAuthor = Object.entries(_.groupBy(blogs, nameOfAuthor))
+  const authorWithMostBlogs = _.maxBy(groupByAuthor, sumOfLikes)
+  return {
+    author: authorWithMostBlogs[0],
+    likes: sumOfLikes(authorWithMostBlogs)
+  }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs,
+  mostLikes
 }
