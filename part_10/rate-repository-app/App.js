8a2573d6bc2cec9f010216954719f0b4fb9118f3
Sani Letchu <sani.letchu@gmail.com>
2022-10-20 16:39:28 +0300

    10.2 done

--- part_10/rate-repository-app/App.js
+++ part_10/rate-repository-app/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container}>∏
       <Text>Hello world!</Text>
       <StatusBar style="auto" />
     </View>
